1cda8220165ac0280c6d140845008c953d088d4b
Javits Arias <javits.arias@gmail.com>
2021-01-12 03:04:13 -0500

    added CSS styling to Navbar component

--- jabbler-client/src/containers/Navbar.js
+++ jabbler-client/src/containers/Navbar.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Logo from '../images/warbler-logo.png';
 
 class Navbar extends Component {
     render(){
         return(
             <nav className="navbar navbar-expand">
                 <div className="container-fluid">
-                    <Link to="/" className="navbar-brand">
-                        <img src="" alt="Jabbler Home" />
-                    </Link>
-                </div>
-                <ul className="nav navbar-nav navbar-right">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">
+                            <img src={Logo} alt="Jabbler Home" />
+                        </Link>
+                    </div>
+                
+                    <ul className="nav navbar-nav navbar-right">
                     <li>
                         <Link to="/signup">Sign up</Link>
                     </li>
                     <li>
                         <Link to="/signin">Log in</Link>
                     </li>
-                </ul>
+                    
+                    </ul>
+                </div>
             </nav>
         );
     }
